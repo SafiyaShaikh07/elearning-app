26671f0054a9615b2e44fe3bf3336930b6f4c5f7
SafiyaShaikh07 <safiyashaikhofficial@gmail.com>
2023-05-24 19:19:59 +0530

    bug fix 122

--- second.java
+++ second.java
@@ -2,4 +2,12 @@ public class New{
     New(){
         System.out.println("0-param const");
     }
+    //BUG- 122
+    public static void m1(){
+        System.out.println("git stash apply");
+    }
+
+
+
 }
+
